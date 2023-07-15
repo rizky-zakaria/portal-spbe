388778a4910400fb8703307ec9f22a78fa4f2ee0
Rizky Zakaria <rizky.zakaria78@gmail.com>
2023-07-16 04:07:17 +0800

    --allow-empty

---
+++ database/migrations/2023_07_15_194104_create_kontens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kontens', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('kategori_id');
+            $table->text('konten');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kontens');
+    }
+};
